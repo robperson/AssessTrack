4ffbfcb1cc6f98d4fa6978d115690b464f52ae84
robperson <rob@robperson.com>
2010-06-08 15:36:11 -0400

    Added CourseTermMember implementation for IBackupItem

---
+++ AssessTrack/Models/CourseTermMember.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AssessTrack.Backup;
+using System.Xml.Linq;
+
+namespace AssessTrack.Models
+{
+    public partial class CourseTermMember : IBackupItem
+    {
+        #region IBackupItem Members
+
+        public Guid objectID
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Xml.Linq.XElement Serialize()
+        {
+            XElement coursetermmember = new XElement("coursetermmember",
+                new XElement("coursetermmemberid", CourseTermMemberID),
+                new XElement("coursetermid", CourseTermID),
+                new XElement("membershipid", MembershipID.ToString()),
+                new XElement("accesslevel", AccessLevel.ToString()));
+            return coursetermmember;
+        }
+
+        public void Deserialize(System.Xml.Linq.XElement source)
+        {
+            try
+            {
+                CourseTermMemberID = new Guid(source.Element("coursetermmemberid").Value);
+                CourseTermID = new Guid(source.Element("coursetermid").Value);
+                MembershipID = new Guid(source.Element("membershipid").Value);
+                AccessLevel = byte.Parse(source.Element("accesslevel").Value);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Failed to deserialize CourseTermMember entity.");
+            }
+        }
+
+        public void Insert(AssessTrackModelClassesDataContext dc)
+        {
+            dc.CourseTermMembers.InsertOnSubmit(this);
+        }
+
+        #endregion
+    }
+}
